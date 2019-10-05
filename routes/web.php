2c6e0c17df3800e64d367a379c00d8efa84c7ef4
Swati Kumari <kumari.swatitcm@gmail.com>
2019-10-05 21:01:06 +0530

    many to many relationship with CRUD operation with attatching,detachind and syncing

---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+use App\User;
+use App\Role;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/insert', function () {
+    $user=User::find(1);
+    // $role = new Role(['name'=>'Administrator']);
+    $user->roles()->save(new Role(['name'=>'Administrator']));
+});
+Route::get('/read', function () {
+    $user=User::findOrFail(1);
+    foreach ($user->roles as $role) {
+    	// dd($role);
+    	echo $role->name;
+    }
+});
+Route::get('/update', function () {
+    $user=User::findOrFail(1);
+    if ($user->has('roles')) {
+        foreach ($user->roles as $role) {
+            if ($role->name=='Administrator') {
+                $role->name = 'subscriber';
+                $role->save();
+            }
+        }
+    }
+});
+Route::get('/delete',function(){
+$user = User::findOrFail(2);
+// $user->roles()->delete();
+
+foreach ($user->roles as $role) {
+    // dd($role);
+    $role->whereId(2)->delete();
+}
+});
+
+Route::get('/attach', function(){
+$user = User::findOrFail(1);
+
+$user->roles()->attach(2);
+
+});
+
+Route::get('/detach', function(){
+$user = User::findOrFail(1);
+
+$user->roles()->detach(2);
+
+});
+
+Route::get('/sync',function(){
+ $user = User::findOrFail(1);
+ $user->roles()->sync([1,2]);
+});
